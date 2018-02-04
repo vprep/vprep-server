10c48a7a42d6f7b624e4483865b5034884c21ad7
Dinesh Sharma <dsharma@upsnap.com>
2018-02-04 18:35:40 +0530

    server changes

---
+++ index.js
@@ -0,0 +1,23 @@
+// Main starting of the application
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const router = require('./router');
+const app = express();
+
+//App Setup
+app.use(morgan('combined'));
+app.use(bodyParser.json({type:'*/*'}));
+router(app);
+
+
+
+
+// Server setup
+const port  = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server listening on:',port);
+
+
--- src/client/client.js
+++
@@ -1,3 +0,0 @@
-//Startup point for the client side application
-
-console.log('Hi there!');
